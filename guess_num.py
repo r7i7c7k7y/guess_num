09b94f8c5cbd16c597184b1c9cee09482f8cf473
Ricky Tsai <r7i7c7k7y@gmail.com>
2018-09-16 21:45:15 +0800

    多顯示猜了幾次

--- guess_num.py
+++ guess_num.py
@@ -1,6 +1,8 @@
 import random
 r = random.randint(1, 100)
+n = 0
 while True:
+	n = n + 1
 	ans = input('猜數字吧 :')
 	ans = int(ans)
 	if r == ans:
@@ -11,4 +13,5 @@ while True:
 			print('太大了拉')
 		else:
 			print('太小了拉')
+print('總共猜了',n,'次')
 	
